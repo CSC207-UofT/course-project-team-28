ee75f2a76bdda70316e023a812d9067ee5b2f4c5
Jiaxi Li <jiaxijacquelin.li@mail.utoronto.ca>
2021-10-11 16:38:08 -0400

    Review

--- src/main/Review.java
+++ src/main/Review.java
@@ -1,2 +1,12 @@
 public class Review {
+    protected NormalUser reviewer;
+    protected Movie movie;
+    protected String reviewcontent;
+
+    // Construct a new review content for the specific movie by the reviewer.
+    public Review(NormalUser reviewer, Movie movie, String reviewcontent){
+        this.reviewer = reviewer;
+        this.movie = movie;
+        this.reviewcontent = reviewcontent;
+    }
 }
