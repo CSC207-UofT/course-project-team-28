6265d42ab8517e72375303d32d547ecd0aaadf4d
Ella-Pan <ellajing.pan@mail.utoronto.ca>
2021-10-11 17:41:02 -0400

    AdminUser edit

--- src/main/AdminUser.java
+++ src/main/AdminUser.java
@@ -1,11 +1,35 @@
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.FileWriter;
+
 public class AdminUser extends User{
+    public FileReader userreader = new FileReader("Normal.txt");;
+    public BufferedReader userlogin = new BufferedReader(userreader);
+    public FileWriter writeuser = new FileWriter("Normal.txt", true);
+
+
     public AdminUser(String username, String password){
         super(username, password);
     }
 
     @Override
     public AdminUser create_account(String username, String password){
-        // override class User's method create_account
+        AdminUser user = new AdminUser(username, password);
+
+        while(!userlogin.readLine().equals("")) {
+            userlogin.readLine();
+        }
+        userlogin.readLine();
+        userlogin.close();
+        writeuser.write("U");
+        writeuser.write("\r\n");
+        writeuser.write("mokila");
+        writeuser.write("\r\n");
+        writeuser.write("123456");
+        writeuser.write("\r\n");
+        writeuser.close();
+
+        return user;
     }
 
 }
