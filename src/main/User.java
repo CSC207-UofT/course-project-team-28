214f72e557125be3ad5577fd382e21975d9a397a
Ella-Pan <ellajing.pan@mail.utoronto.ca>
2021-10-11 15:19:23 -0400

    User

--- src/main/User.java
+++ src/main/User.java
@@ -1,2 +1,13 @@
 public class User {
+    protected String username;
+    protected String password;
+
+    public User(String username, String password){
+        this.username = username;
+        this.password = password;
+    }
+    public void create_account(String username, String password){
+        // create a new account and write into file, it should be only called when UserManger considers the username
+        // and password is valid. Thus, Input are all valid.
+    }
 }
