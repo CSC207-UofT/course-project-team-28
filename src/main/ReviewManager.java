5dafe3469202011c46972cc39b0b2e7af184386c
jac02a <jackiexh1@126.com>
2021-10-12 13:04:56 -0400

    v1

--- src/main/ReviewManager.java
+++ src/main/ReviewManager.java
@@ -3,9 +3,9 @@ import java.util.ArrayList;
 
 
 public class ReviewManager {
-    private HashMap<String, ArrayList<Review>> MovietoRevs;
-    private HashMap<String, ArrayList<Review>> UsertoRevs;
-    private ArrayList<Review> lst;
+    private final HashMap<String, ArrayList<Review>> MovietoRevs;
+    private final HashMap<String, ArrayList<Review>> UsertoRevs;
+    private final ArrayList<Review> lst;
 
 
     public ReviewManager(){
@@ -19,7 +19,7 @@ public class ReviewManager {
      * Takes the username of NormalUser and return a list of reviews written by the user.
      */
     public ArrayList<Review> RevsofUser(String username){
-
+        return this.UsertoRevs.get(username);
     }
 
     /**
@@ -27,7 +27,7 @@ public class ReviewManager {
      * Takes the name of Movie and return a list of reviews for the Movie.
      */
     public ArrayList<Review> RevsofMovie(String moviename){
-
+        return this.MovietoRevs.get(moviename);
     }
 
     /**
@@ -35,20 +35,54 @@ public class ReviewManager {
      * Return ture iff the review has been successfully created and added to the txt file.
      */
     public boolean write_review(String uname, String mname, String content) {
+        boolean write = true;
         Review rev = new Review(uname, mname, content);
 
-        // add the review in file
+        // add the review in file by a helper method
 
-        // update MovietoRevs
+
+        write = add_mr(mname, rev) && write; // update MovietoRevs
+
+        write = add_ur(uname, rev) && write; // update UsertoRevs
+
+        // update lst
+        this.lst.add(rev);
+
+        return write;
+    }
+
+    /**
+     * update MovietoRevs by write_review, return ture iff successfully updated.
+     */
+    private boolean add_mr(String mname, Review rev){
         if (this.MovietoRevs.containsKey(mname)) {
             this.MovietoRevs.get(mname).add(rev);
         }
-        else {
+        else if (! this.MovietoRevs.containsKey(mname)) {
             ArrayList<Review> lst = new ArrayList<>();
             lst.add(rev);
             this.MovietoRevs.put(mname, lst);
         }
+        else {return false;}
 
+        return true;
+    }
+
+    /**
+     * update MovietoRevs by write_review, return ture iff successfully updated.
+     */
+    private boolean add_ur(String uname, Review rev){
+        if (this.UsertoRevs.containsKey(uname)) {
+            this.UsertoRevs.get(uname).add(rev);
+        }
+        else if (! this.UsertoRevs.containsKey(uname)) {
+            ArrayList<Review> lst = new ArrayList<>();
+            lst.add(rev);
+            this.UsertoRevs.put(uname, lst);
+        }
+        else {return false;}
+
+        return true;
     }
 
     /**
