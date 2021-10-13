004139e48a3f8534755de0d4ad050e314a55694f
yuxuanli01 <dorislyx.li@mail.utoronto.ca>
2021-10-13 19:20:33 -0400

    nnnn

--- src/main/InputProcessor.java
+++ src/main/InputProcessor.java
@@ -4,5 +4,12 @@ public class InputProcessor {
     static private MovieManager mov_mana;
     static private UserManager user_mana;
 
-
+    private boolean is_alphanumeric(String s) {
+        for (int i = 0; i < s.length(); i ++) {
+            if (! ((char) s[i].isAlphabetic() / (char) s[i].isDigit())) {
+                return false;
+            }
+        }
+        return true;
+    }
 }
