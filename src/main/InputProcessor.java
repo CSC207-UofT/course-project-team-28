1844bab0583f663c3a2dd1bb02d6fb8a14a04a78
jac02a <jackiexh1@126.com>
2021-10-13 19:50:02 -0400

    v1

--- src/main/InputProcessor.java
+++ src/main/InputProcessor.java
@@ -4,6 +4,12 @@ public class InputProcessor {
     static private MovieManager mov_mana;
     static private UserManager user_mana;
 
+    public InputProcessor(){
+        rev_mana = new ReviewManager();
+        mov_mana = new MovieManager();
+        user_mana = new UserManager();
+    }
+
     private boolean is_alphanumeric(String s) {
         for (int i = 0; i < s.length(); i ++) {
             if (! (Character.isAlphabetic(s.charAt(i)) || Character.isDigit(s.charAt(i)))) {
@@ -12,4 +18,25 @@ public class InputProcessor {
         }
         return true;
     }
+
+    /**
+     * create normal user iff the provided username and password is legal
+     * username and password must only contain numbers or letters, and username must be unique
+     */
+    public boolean create_norm_user(String un, String pass) {
+        if (! this.is_alphanumeric(un)){
+            return false;
+        }
+        else if (! this.is_alphanumeric(pass)){
+            return false;
+        }
+        else if (! this.is_alphanumeric(pass)){
+            return false;
+        }
+        else{
+
+        }
+    }
+
+
 }
