5328780d4be3a13a56670a17ed325908001b843e
yuxuanli01 <dorislyx.li@mail.utoronto.ca>
2021-10-13 20:31:35 -0400

    nnnn

--- src/main/InputProcessor.java
+++ src/main/InputProcessor.java
@@ -1,3 +1,6 @@
+import java.util.List;
+import java.util.Objects;
+
 public class InputProcessor {
     // delete or add movie(Admin User), call MovieManger
     static private ReviewManager rev_mana;
@@ -10,10 +13,37 @@ public class InputProcessor {
         user_mana = new UserManager();
     }
 
+    /**
+     * Return true if the given string (username or password) is alphanumeric.
+     */
     private boolean is_alphanumeric(String s) {
         for (int i = 0; i < s.length(); i ++) {
             if (! (Character.isAlphabetic(s.charAt(i)) || Character.isDigit(s.charAt(i)))) {
                 return false;
+            }
+        }
+        return true;
+    }
+
+    /**
+     * Return false if the given string (username) is used in the same usertype before.
+     * Return true if the given string (username) is available to use.
+     */
+    private boolean un_canbeused(String s, String usertype) {
+        List<String> listofadmname = user_mana.get_listadminusername();
+        List<String> listofnormname = user_mana.get_listnormalusername();
+        if (Objects.equals(usertype, "A")) {
+            for (String eachadmname: listofadmname) {
+                if (Objects.equals(s, eachadmname)) {
+                    return false;
+                }
+            }
+        }
+        else {
+            for (String eachnormname: listofnormname) {
+                if (Objects.equals(s, eachnormname)) {
+                    return false;
+                }
             }
         }
         return true;
