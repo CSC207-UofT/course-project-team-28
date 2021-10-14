ec04549364b78c4eaecd92791f7dd7c056461496
gy2075338364 <andi.han@mail.utoronto.ca>
2021-10-14 11:40:28 -0400

    Movie part done, something's wrong with "run" in IntelliJ, can't test :(

--- src/main/WriteMovie.java
+++ src/main/WriteMovie.java
@@ -3,6 +3,7 @@ import java.nio.file.FileSystems;
 import java.nio.file.Path;
 import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.Objects;
 import java.util.Properties;
 
 /**
@@ -31,8 +32,39 @@ public class WriteMovie implements WriteFile{
         writemovie.close();
         Properties properties = new Properties();
         properties.putAll(((Movie) movie).moviereviews);
-        properties.store(new FileOutputStream(p1.toString() + "\\main\\Moviereview\\" + ((Movie) movie).moviename + " reviews.txt"), null);
+        FileOutputStream fos;
+        fos = new FileOutputStream(p1.toString() + "\\main\\Moviereview\\" + ((Movie) movie).moviename + " reviews.properties");
+        properties.store(fos, null);
+        fos.close();
     }
+
+
+    public void add_review(Review review) throws IOException {
+        Path p1 = FileSystems.getDefault().getPath("").toAbsolutePath();
+        FileOutputStream fos;
+        Properties properties = new Properties();
+        properties.put(review.review_content, review.reviewer);
+        File file = new File(p1.toString() + "\\main\\Moviereview\\" + review.movie + " reviews.properties");
+        fos = new FileOutputStream(file, true);
+        properties.store(fos, null);
+        fos.close();
+    }
+
+    /*
+    public void add_review(Review review) throws IOException {
+        Path p1 = FileSystems.getDefault().getPath("").toAbsolutePath();
+        File file = new File(p1.toString() + "\\main\\Moviereview\\" + review.movie + " reviews.properties");
+        FileOutputStream fos;
+        FileInputStream fls;
+        fls = new FileInputStream(file);
+        Properties properties = new Properties();
+        properties.load(fls);
+        properties.setProperty(review.review_content, review.reviewer);
+        fos = new FileOutputStream(file);
+        properties.store(fos, null);
+        fos.close();
+    }
+    */
 
     /**
      * Read Moviedata and Moviereviews two folders, create obejct for each movie and return a two-dimensional array.
@@ -48,19 +80,20 @@ public class WriteMovie implements WriteFile{
         String[] lstOfMovie = MoviePath.list();// get all the file name in Moviedata folder
         String[] lstOfMovieReview = MoviereviewPath.list();// get all the file name in Moviereview folder
 
-        ArrayList<Object> Movie_lst = new ArrayList<Object>(); // the list that will be returned, containing Movies.
+        ArrayList<Object> Movie_lst = new ArrayList<>(); // the list that will be returned, containing Movies.
 
         if(lstOfMovie == null){
-            return new ArrayList<Object>();
+            return new ArrayList<>();
         }
         else{
             for(String m: lstOfMovie) {
                 ArrayList<String> lst = read_file(p1, m, "Moviedata");
-                HashMap moviereview = null; // initialise a HashMap
+                HashMap<Object, Object> moviereview = null; // initialise a HashMap
+                assert lstOfMovieReview != null;
                 for (String mr : lstOfMovieReview) {
-                    if (mr == m + " reviews") {
+                    if (Objects.equals(mr, m + " reviews")) {
                         Properties properties = new Properties();
-                        FileInputStream movier = new FileInputStream(MoviereviewPath.toString() + mr + ".txt");
+                        FileInputStream movier = new FileInputStream(MoviereviewPath.toString() + mr + ".properties");
                         properties.load(movier);
                         moviereview = new HashMap<Object, Object>(properties);
                     } // Find the corresponding review file for movie and change the HashMap created to the stored one
@@ -76,7 +109,7 @@ public class WriteMovie implements WriteFile{
     public void delete_file(Object movie) throws IOException {
         Path p1 = FileSystems.getDefault().getPath("").toAbsolutePath();
         File moviefile = new File(p1.toString() + "\\main\\Moviedata\\" + ((Movie) movie).moviename + ".txt");
-        File moviereviewfile = new File(p1.toString() + "\\main\\Moviereview\\" + ((Movie) movie).moviename + " reviews.txt");
+        File moviereviewfile = new File(p1.toString() + "\\main\\Moviereview\\" + ((Movie) movie).moviename + " reviews.properties");
         Boolean a = moviefile.delete();
         Boolean b = moviereviewfile.delete();
     }
