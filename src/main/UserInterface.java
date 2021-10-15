c33de9eaf826020178d765766ff309955e9901da
SayaJPF <joanna.pan@mail.utoronto.ca>
2021-10-15 18:34:17 -0400

    UI profile & playlist & review added

--- src/main/UserInterface.java
+++ src/main/UserInterface.java
@@ -23,6 +23,15 @@ public class UserInterface {
             System.out.println("Please enter your password:");
             String password = scanner.nextLine();
             controller.login(username,password);
+            System.out.println("Please enter your phone number");
+            String contactinfo = scanner.nextLine();
+            controller.edit_profile(contactinfo);
+            System.out.println("Enter the name of the movie you'd like to add to your playlist");
+            String moviesname = scanner.nextLine();
+            controller.like_movie(moviesname);
+            System.out.println("Enter your review of the movie");
+            String review_content = scanner.nextLine();
+            controller.write_review(moviesname, review_content);
 
         }else if (choice.equals("Admin register")){
             AdminInputProcessor controller = new AdminInputProcessor();
