8bb6960b821d30977dc8df3c04124d91c6f57409
gy2075338364 <andi.han@mail.utoronto.ca>
2021-10-16 00:25:47 -0400

    Movie part done, tested

--- src/main/Movie.java
+++ src/main/Movie.java
@@ -28,27 +28,22 @@ public class Movie {
         this.Likes = i;
     }
 
+    /**
+     * Method AddReview take only a parameter of Class review, which will store the
+     * review content as key and the corresponding user as value. Once a review is written, it
+     * should first be stored in the Review Class and then Movie Class will take data from Review
+     * Class and store them.
+     */
+
     public void AddReview(Review review){
         this.moviereviews.put(review.review_content, review.reviewer);
     }
 
     /**
-     * Method AddReview take only a parameter of Class review, which will store the
-     * review content as key and the corresponding user as value. Once a review is written, it
-     * should first be stored in the Review Class and then Movie Class will take data from Review
-     * Class and store them
-     * @return
+     * Getter Methods for Movie Class.
      */
 
     public String toString() {return this.moviename; }
-
-    public void AddLike(){
-        this.Likes += 1;
-    }
-
-    public void UndoLike(){
-        this.Likes -= 1;
-    }
 
     public String getMoviename(){
         return this.moviename;
@@ -61,6 +56,22 @@ public class Movie {
     public HashMap<Object, Object> getMovieReviews() {return this.moviereviews; }
 
     public int getLikes() {return this.Likes; }
+
+    /**
+     * Add or retrieve a like to a movie.
+     */
+
+    public void AddLike(){
+        this.Likes += 1;
+    }
+
+    public void UndoLike(){
+        this.Likes -= 1;
+    }
+
+    /**
+     * Get only the review contents of a movie and return them in an ArrayList.
+     */
 
     public ArrayList<String> getReviewsContnet(){
         ArrayList<String> contents = new ArrayList<String>();
