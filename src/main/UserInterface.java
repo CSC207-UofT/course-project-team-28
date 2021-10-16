dfbaaaf5612e3149e3bcfb3653130cc4b744d578
SayaJPF <joanna.pan@mail.utoronto.ca>
2021-10-16 14:43:34 -0400

    UI update !!!! -  added program exits reminder

--- src/main/UserInterface.java
+++ src/main/UserInterface.java
@@ -70,10 +70,19 @@ public class UserInterface {
                     System.out.println("Please enter your phone number");
                     String contactinfo = scanner.nextLine();
                     controller.edit_profile(contactinfo);
+                    System.out.println("your new profile is: " + controller.profile_page(username));
                 } else if (choise.equals("undo-like the movie")){
-                    System.out.println("type the movie name you would like to remove from your playlist");
-                    String mvname = scanner.nextLine();
-                    controller.undo_like(mvname);
+                    try {
+                        System.out.println("type the movie name you would like to remove from your playlist");
+                        String mvname = scanner.nextLine();
+                        controller.undo_like(mvname);
+                        System.out.println("your new profile is " + controller.profile_page(username));
+                        System.out.println("Movie successfully removed.");
+                        System.out.println("program exits.");
+                    } catch (Exception e) {
+                        System.out.println("Movie does not exit");
+                        System.out.println("program exits.");
+                    }
                 } else {
                     System.out.println("wrong input");
                     System.exit(1);
@@ -98,16 +107,23 @@ public class UserInterface {
                     System.out.println("Enter your review of the movie");
                     String review_content = scanner.nextLine();
                     controller.write_review(moviename, review_content);
+                    System.out.println("Check your review: " + controller.movie_profile(moviename));
+                    System.out.println("program exits.");
                 } else if (c.equals("Like the movie")){
                     controller.like_movie(moviename);
+                    System.out.println("Check your likes: " + controller.movie_profile(moviename));
+                    System.out.println("program exits.");
                 } else if (c.equals("Exit program")){
+                    System.out.println("Program exits.");
                     System.exit(0);
                 } else {
                     System.out.println("wrong input");
+                    System.out.println("Program exits.");
                     System.exit(1);
                 }
             } else {
                 System.out.println("wrong input");
+                System.out.println("Program exits.");
                 System.exit(1);
             }
         // if user chooses to login as normal user
@@ -153,7 +169,7 @@ public class UserInterface {
                     System.out.println("Please enter your phone number");
                     String contactinfo = scanner.nextLine();
                     controller.edit_profile(contactinfo);
-                    System.out.println(controller.profile_page(username));
+                    System.out.println("your new profile is: " + controller.profile_page(username));
                 } else if (choise.equals("undo-like the movie")){
                     try {
                         System.out.println("type the movie name you would like to remove from your playlist");
@@ -161,11 +177,14 @@ public class UserInterface {
                         controller.undo_like(mvname);
                         System.out.println("your new profile is " + controller.profile_page(username));
                         System.out.println("Movie successfully removed.");
+                        System.out.println("program exits.");
                     } catch (Exception e) {
                         System.out.println("Movie does not exit");
+                        System.out.println("program exits.");
                     }
                 } else {
                     System.out.println("wrong input");
+                    System.out.println("program exits.");
                     System.exit(1);
                 }
             } else if (choose.equals("Search")){
@@ -187,12 +206,18 @@ public class UserInterface {
                         System.out.println("Enter your review of the movie");
                         String review_content = scanner.nextLine();
                         controller.write_review(moviename, review_content);
+                        System.out.println("Check your review: " + controller.movie_profile(moviename));
+                        System.out.println("program exits.");
                     } else if (c.equals("Like the movie")) {
                         controller.like_movie(moviename);
+                        System.out.println("Check your likes: " + controller.movie_profile(moviename));
+                        System.out.println("program exits.");
                     } else if (c.equals("Exit program")) {
+                        System.out.println("program exits.");
                         System.exit(0);
                     } else {
                         System.out.println("wrong input");
+                        System.out.println("program exits.");
                         System.exit(1);
                     }
                 } catch (Exception e) {
@@ -240,12 +265,15 @@ public class UserInterface {
                 System.out.println("Please enter the link of the movie");
                 String movie_link = scanner.nextLine();
                 controller.upload_movie(movie_name, movie_link);
+                System.out.println("Movie added");
+                System.out.println("program exits.");
             //} else if (choose.equals("Delete movie")){
             //    System.out.println("Please enter the name of the movie");
             //    String movie_name = scanner.nextLine();
             //    controller.delete_movie(movie_name);
             } else {
                 System.out.println("wrong input");
+                System.out.println("program exits.");
                 System.exit(1);
             }
 
@@ -273,13 +301,13 @@ public class UserInterface {
             }
             System.out.println("Login successful.");
 
-            System.out.println("Enter 'Upload movie' or 'Delete movie'.");
+            System.out.println("Enter 'Upload movie'.");
             String choose = scanner.nextLine();
 
-            String[] option_4= new String[]{"Upload movie", "Delete movie"};
+            String[] option_4= new String[]{"Upload movie"};
             List<String> options_4 = new ArrayList<>(Arrays.asList(option_4));
             while (!options_4.contains(choose)) {
-                System.out.println("Please reenter 'Upload movie' or 'Delete movie':");
+                System.out.println("Please reenter 'Upload movie':");
                 choose = scanner.nextLine();
             }
 
@@ -289,17 +317,21 @@ public class UserInterface {
                 System.out.println("Please enter the link of the movie");
                 String movie_link = scanner.nextLine();
                 controller.upload_movie(movie_name, movie_link);
-            } else if (choose.equals("Delete movie")){
-                System.out.println("Please enter the name of the movie");
-                String movie_name = scanner.nextLine();
-                controller.delete_movie(movie_name);
+                System.out.println("Movie added");
+                System.out.println("program exits.");
+            //} else if (choose.equals("Delete movie")){
+            //    System.out.println("Please enter the name of the movie");
+            //    String movie_name = scanner.nextLine();
+            //    controller.delete_movie(movie_name);
             } else {
                 System.out.println("wrong input");
+                System.out.println("program exits.");
                 System.exit(1);
             }
 
         }else {
             System.out.println("wrong input");
+            System.out.println("program exits.");
             System.exit(1);
         }
     }
