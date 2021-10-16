182f9d7d1d6e01106210eb89acda70b0ecf76e6f
Ella-Pan <ellajing.pan@mail.utoronto.ca>
2021-10-16 13:47:12 -0400

    Add moviereview Folder

--- src/main/demoMain.java
+++ src/main/demoMain.java
@@ -8,6 +8,11 @@ public class Demomain {
 //        //wr.create_file(r);
 //        ArrayList<Review> re = wr.get_object_from_file();
 //        System.out.println(re);
+//         WriteMovie wm = new WriteMovie();
+//         Movie m = new Movie("Apple", "jsdhahduia");
+//         wm.create_file(m);
+//         Review r = new Review("Ella", "Apple", "hahahahah", 1);
+//         wm.add_review_to_file(r);
 
     }
 }
