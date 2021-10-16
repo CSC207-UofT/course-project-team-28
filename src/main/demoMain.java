6bcf18d28579f67464cea33f99ee64f7081a72eb
Ella-Pan <ellajing.pan@mail.utoronto.ca>
2021-10-16 14:03:09 -0400

    Delete all files

--- src/main/demoMain.java
+++ src/main/demoMain.java
@@ -3,16 +3,10 @@ import java.util.ArrayList;
 
 public class Demomain {
     public static void main(String[] args) throws IOException {
-//        WriteReview wr = new WriteReview();
-//        //Review r = new Review("Doris", "Banana", "hhhhh", 5);
-//        //wr.create_file(r);
-//        ArrayList<Review> re = wr.get_object_from_file();
-//        System.out.println(re);
-//         WriteMovie wm = new WriteMovie();
-//         Movie m = new Movie("Apple", "jsdhahduia");
-//         wm.create_file(m);
-//         Review r = new Review("Ella", "Apple", "hahahahah", 1);
-//         wm.add_review_to_file(r);
+//        WriteUser wu = new WriteUser();
+//        wu.undo_like_readandwrite("Banana", "Ella");
+
+
 
     }
 }
