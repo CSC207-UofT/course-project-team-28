04b0e308b9ab0c095f4d98cb392783cb8d875333
jac02a <jackiexh1@126.com>
2021-10-16 20:18:13 -0400

    add empty_playlist

--- src/main/NormalInputProcessor.java
+++ src/main/NormalInputProcessor.java
@@ -145,18 +145,25 @@ public class NormalInputProcessor {
         }
     }
 
+    /**
+     * return ture iff the user's playlist is empty.
+     */
+    public boolean empty_playlist() {
+        Object[] user_info = user_mana.getUserInfoList(curr_nuname, "NormalUser");
+        ArrayList<String> user_playlist = (ArrayList<String>) user_info[3];
+        if (user_playlist.isEmpty()) {
+            return true;
+        } else {
+            return false;
+        }
+    }
 
     /**
      * Given a String moviename, undo like.
      * return ture iff added successfully.
      */
     public boolean undo_like(String moviename) throws IOException {
-        Object[] user_info = user_mana.getUserInfoList(curr_nuname, "NormalUser");
-        ArrayList<String> user_playlist = (ArrayList<String>) user_info[3];
-        if (user_playlist.isEmpty()) {
-            return false;
-        }
-        else if (user_playlist.contains(moviename)){
+        if (user_playlist.contains(moviename)){
             user_mana.undo_like(this.curr_nuname, moviename);
             mov_mana.undolike_movie(moviename);
             return true;
