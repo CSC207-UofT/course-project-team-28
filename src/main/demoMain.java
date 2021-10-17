dcb481b4c8b783e538ad87dbb4defab476eac693
gy2075338364 <andi.han@mail.utoronto.ca>
2021-10-16 20:05:49 -0400

    Movie part done, tested

--- src/main/demoMain.java
+++ src/main/demoMain.java
@@ -1,13 +1,6 @@
-import java.io.IOException;
-import java.util.ArrayList;
-
-public class Demomain {
-    public static void main(String[] args) throws IOException {
-//        WriteReview wr = new WriteReview();
-//        //Review r = new Review("Doris", "Banana", "hhhhh", 5);
-//        //wr.create_file(r);
-//        ArrayList<Review> re = wr.get_object_from_file();
-//        System.out.println(re);
+class Demomain {
+    public static void main(String[] args) {
+        UserInterface ui = new UserInterface();
 
     }
 }
