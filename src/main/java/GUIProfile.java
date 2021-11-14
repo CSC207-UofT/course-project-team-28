9ab86f357004e243d81f0de50209645f1485f003
SayaJPF <joanna.pan@mail.utoronto.ca>
2021-11-14 01:45:09 -0500

    GUIProfile updated

--- src/main/java/GUIProfile.java
+++ src/main/java/GUIProfile.java
@@ -1,17 +1,15 @@
 import javax.swing.*;
 import java.awt.*;
 
+
 public class GUIProfile extends View {
-    JTabbedPane jTabbedPane;
-    JLabel usernameLabel;
-    JLabel coinLabel;
-    JPanel panel1;
-    JPanel panel2;
-    JPanel panel3;
-    Font font1 = new Font("SansSerif", Font.BOLD, 20);
+    private final JTabbedPane jTabbedPane;
+    private final JPanel panel1;
+    private final JPanel panel2;
+    private final JPanel panel3;
+    Font font1 = new Font("SansSerif", Font.BOLD, 30);
     public GUIProfile(){
         panel1 = new JPanel();
-        panel1.setLayout(null);
         panel2 = new JPanel();
         panel3 = new JPanel();
         jTabbedPane = new JTabbedPane();
@@ -19,13 +17,31 @@ public class GUIProfile extends View {
         jTabbedPane.add("User Profile",panel1);
         jTabbedPane.add("Playlist",panel2);
         jTabbedPane.add("Category",panel3);
-        usernameLabel = new JLabel("Username: " + IM.nucontroller.profile_page(userName).get(0));
+        PlaceThingsOnP1(panel1);
+    }
+    private void PlaceThingsOnP1(JPanel p1){
+        p1.setLayout(null);
+        JLabel usernameLabel = new JLabel("Username: " + IM.nucontroller.profile_page(userName).get(0));
         usernameLabel.setFont(font1);
-        usernameLabel.setBounds(20,20,300,200);
-        coinLabel = new JLabel("number of coins: " + "300");
-        coinLabel.setBounds(200, 20, 300, 200);
-        panel1.add(usernameLabel);
-        panel1.add(coinLabel);
+        usernameLabel.setBounds(20,20,700,200);
+        JLabel coinLabel = new JLabel("number of coins: " + "300");//IM.nucontroller.profile_page(userName).get(4)
+        coinLabel.setBounds(20, 60, 300, 200);
+        JLabel contactInfoLabel = new JLabel("Contact info: " + IM.nucontroller.profile_page(userName).get(1));
+        contactInfoLabel.setBounds(20, 90, 700, 200);
+        JLabel description = new JLabel("User description: " + IM.nucontroller.profile_page(userName).get(2));
+        description.setBounds(20, 120, 700, 200);
+        JButton EditContactInfo = new JButton("Edit");
+        EditContactInfo.setBounds(150,180,60,20);
+        p1.add(usernameLabel);
+        p1.add(coinLabel);
+        p1.add(contactInfoLabel);
+        p1.add(EditContactInfo);
+        p1.add(description);
+    }
+    private void PlaceThingsOnP2(JPanel p2){
+        p2.setLayout(null);
+        JList PlayList = new JList();
+
     }
 
     @Override
