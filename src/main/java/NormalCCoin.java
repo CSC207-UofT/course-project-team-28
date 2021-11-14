6fc1959f86869eb28047df608babd38ba93f0f47
jac02a <jackiexh1@126.com>
2021-11-14 17:40:19 -0500

    add two methods in NormalCCoin.

--- src/main/java/NormalCCoin.java
+++ src/main/java/NormalCCoin.java
@@ -5,4 +5,23 @@ public class NormalCCoin extends NormalController{
     }
 
 
+    /**
+     * Return true if the number of coins that the user has is greater or equal to 1.
+     * Given the username and reviewid, update the number of coins that the user has,
+     * and update the number of coins that the review earns.
+     */
+    public boolean giveCoinToRev(int reviewId){
+        return coinMana.GiveCoinToReview(currNuname, reviewId);
+    }
+
+
+    /**
+     * Update the number of coins that the user has after a review.
+     * Note: number of coins of that the user +1.
+     */
+    public void EarnCoinAfterWriteRev(){
+        coinMana.EarnCoinAfterReview(currNuname);
+    }
+
+
 }
