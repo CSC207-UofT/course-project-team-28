55a47f2e57b51542c037bb85a76fa660bd58a52a
gy2075338364 <andi.han@mail.utoronto.ca>
2021-11-29 00:18:43 -0500

    Added Picture File to res and New subPackage "Presenter". Polished GUI Main Page.

--- src/main/java/Framework/GUI/GUIMain.java
+++ src/main/java/Framework/GUI/GUIMain.java
@@ -4,8 +4,10 @@ import Framework.DataAccess.WriteMovie;
 import Framework.DataAccess.WriteReview;
 import Framework.DataAccess.WriteUser;
 import InterfaceAdapter.InstanceMain;
+import Framework.Presenter.PicPresenter;
 
 import javax.swing.*;
+import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
@@ -49,13 +51,30 @@ public class GUIMain extends View {
     protected void UpdateText() {
 
     }
+    protected void addComponentToP1() {
+        ImageIcon bg = new ImageIcon(PicPresenter.getPic("bg.gif"));
+        bg.setImage(bg.getImage().getScaledInstance(810, 1440, Image.SCALE_DEFAULT));
+        JLabel jbg = new JLabel(bg);
+        jbg.add(registerButton);
+        registerButton.setBounds(100, 600,250,100);
+        jbg.add(loginButton);
+        loginButton.setBounds(460, 600,250,100);
+        jbg.add(adminRegisterButton);
+        adminRegisterButton.setBounds(100, 725, 250, 100);
+        jbg.add(adminLoginButton);
+        adminLoginButton.setBounds(460, 725, 250, 100);
+        panel1.add(jbg);
+    }
+
 
     @Override
     protected JFrame getFrame() {
         JFrame jFrame = super.getFrame();
         jFrame.setTitle("Our App");
-        jFrame.setSize(300, 120);
-        jFrame.setContentPane(panel1);
+        jFrame.setSize(810, 900);
+        jFrame.setVisible(true);
+        addComponentToP1();
+        jFrame.add(panel1);
         jFrame.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
         return jFrame;
     }
--- src/main/java/Framework/GUI/GUIProfile.java
+++ src/main/java/Framework/GUI/GUIProfile.java
@@ -1,8 +1,11 @@
 package Framework.GUI;
 
 import InterfaceAdapter.InstanceMain;
+import Framework.Presenter.PicPresenter;
 
 import javax.swing.*;
+import javax.swing.border.Border;
+import javax.swing.text.StyledDocument;
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
@@ -12,26 +15,70 @@ import java.util.ArrayList;
 public class GUIProfile extends View {
     public JLabel contactInfoLabel;
     private JLabel coinLabel;
-    private final JTabbedPane jTabbedPane;
+    /*private final JTabbedPane jTabbedPane;*/
     private final JPanel panel1;
+    /*
     private final JPanel panel2;
     private final JPanel panel3;
+    */
     Font font1 = new Font("SansSerif", Font.BOLD, 30);
     public GUIProfile(View previous){
         super(previous);
         panel1 = new JPanel();
+        /*
         panel2 = new JPanel();
         panel3 = new JPanel();
         jTabbedPane = new JTabbedPane();
         jTabbedPane.setBounds(35,30,700,500);
-        jTabbedPane.add("Core.User Profile",panel1);
+        jTabbedPane.add("User Profile",panel1);
         jTabbedPane.add("Playlist",panel2);
         jTabbedPane.add("Category",panel3);
+         */
         PlaceThingsOnP1(panel1);
+        /*
         PlaceThingsOnP2(panel2);
+         */
     }
     private void PlaceThingsOnP1(JPanel p1){
         p1.setLayout(null);
+        Border b = BorderFactory.createLineBorder(Color.BLACK, 1);
+        p1.setBorder(b);
+        p1.setBounds(20,20, 380,820);
+        JLabel usernameLabel = new JLabel((String)InstanceMain.getNormalCUser().profilePage(userName)[0]);
+        usernameLabel.setFont(font1);
+        usernameLabel.setBounds(20,20,700,200);
+        coinLabel = new JLabel("coins: " + InstanceMain.getNormalCUser().profilePage(userName)[5]);
+        coinLabel.setBounds(20, 60, 300, 200);
+        contactInfoLabel = new JLabel();
+        contactInfoLabel.setText((String)InstanceMain.getNormalCUser().profilePage(userName)[2]);
+        contactInfoLabel.setBounds(20, 90, 700, 200);
+        ImageIcon icon = new ImageIcon(PicPresenter.getPic("5.jpg"));
+        JLabel i = new JLabel("Image and Text", icon, JLabel.CENTER);
+        i.setBounds(20, 200, 300, 200);
+        JTextArea description = new JTextArea((String)InstanceMain.getNormalCUser().profilePage(userName)[3]);
+        description.setEditable(false);
+        description.setBounds(40, 480, 300, 200);
+        JButton EditContactInfo = new JButton("Edit");
+        EditContactInfo.setBounds(280,180,60,20);
+        EditContactInfo.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent actionEvent) {
+                OnButtonClick(actionEvent);
+            }
+        });
+        UpdateText();
+        p1.add(usernameLabel);
+        p1.add(coinLabel);
+        p1.add(contactInfoLabel);
+        p1.add(EditContactInfo);
+        p1.add(description);
+        p1.add(i);
+    }
+
+    /*
+    private void PlaceThingsOnP1(JPanel p2){
+        p2.setLayout(null);
+        p2.setBounds(640,20, 600,450);
         JLabel usernameLabel = new JLabel("Username: " + InstanceMain.getNormalCUser().profilePage(userName)[0]);
         usernameLabel.setFont(font1);
         usernameLabel.setBounds(20,20,700,200);
@@ -40,7 +87,7 @@ public class GUIProfile extends View {
         contactInfoLabel = new JLabel();
         contactInfoLabel.setText("Contact Info: " + InstanceMain.getNormalCUser().profilePage(userName)[2]);
         contactInfoLabel.setBounds(20, 90, 700, 200);
-        JLabel description = new JLabel("Core.User description: " + InstanceMain.getNormalCUser().profilePage(userName)[3]);
+        JLabel description = new JLabel("User description: " + InstanceMain.getNormalCUser().profilePage(userName)[3]);
         description.setBounds(20, 120, 700, 200);
         JButton EditContactInfo = new JButton("Edit");
         EditContactInfo.setBounds(280,180,60,20);
@@ -57,6 +104,8 @@ public class GUIProfile extends View {
         p1.add(EditContactInfo);
         p1.add(description);
     }
+    */
+
     private void PlaceThingsOnP2(JPanel p2){
         p2.setLayout(null);
         ArrayList<String> playList = (ArrayList<String>) InstanceMain.getNormalCUser().profilePage(userName)[6];
@@ -81,8 +130,11 @@ public class GUIProfile extends View {
     public JFrame getFrame() {
         JFrame frame = super.getFrame();
         frame.setTitle("Your Page");
+        /*
         frame.add(jTabbedPane);
-        frame.setSize(800,600);
+        */
+        frame.add(panel1);
+        frame.setSize(1200,900);
         frame.setLayout(null);
         frame.setVisible(true);
         frame.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
