2572c871cc6b0c3696a1ef520fb35ce84786190a
jac02a <jackiexh1@126.com>
2021-12-05 23:35:00 -0500

    Add documentation in NormalController, and deleted unused method setCurrNormalName.

--- src/main/java/InterfaceAdapter/Controller/NormalController.java
+++ src/main/java/InterfaceAdapter/Controller/NormalController.java
@@ -1,24 +1,20 @@
 package InterfaceAdapter.Controller;
 
-import UseCase.CoinManager;
-import UseCase.MovieManager;
-import UseCase.ReviewManager;
-import UseCase.UserManager;
-
+/**
+ * NormalController (InterfaceAdapter)
+ * Parent class of all normal user controllers.
+ */
 public class NormalController {
     protected String currNormalName;
-    // delete or add movie(Admin Core.User), call MovieManger
 
-    public void setCurrNormalName(String currNuname) {
-        this.currNormalName = currNuname;
-    }
 
     /**
-     * Return true if the given string (username or password) is alphanumeric,
-     * also it is nonempty.
+     * @param s the string it checks
+     * @return true if the given string (username or password) is alphanumeric,
+     *         also it is nonempty.
      */
-    public boolean isNonemptyalphanumeric(String s) {
-        if (s.length() < 1) {
+    public boolean isNonEmptyAlphanumeric(String s) {
+        if (s.length() == 0) {
             return false;
         }
         for (int i = 0; i < s.length(); i++) {
