c00ae35ca9ab130d83c5798bbe7ce7fca2e103b5
jac02a <jackiexh1@126.com>
2021-12-05 22:47:02 -0500

    Add documentation for NormalCMovie

--- src/main/java/InterfaceAdapter/Controller/NormalCMovie.java
+++ src/main/java/InterfaceAdapter/Controller/NormalCMovie.java
@@ -4,17 +4,19 @@ import InterfaceAdapter.InstanceMain;
 
 import java.util.ArrayList;
 
+/**
+ * NormalCMovie (InterfaceAdapter)
+ * Controller responsible for movie-related operations.
+ */
 public class NormalCMovie extends NormalController{
 
     public NormalCMovie() {
         super();
     }
 
-
-    //movie
-
     /**
-     * check if the moviename exists
+     * check if the movieName exists
+     * @param movieName the name of the movie.
      * @return ture iff the movie exists
      */
     public boolean ifMovieExist(String movieName){
@@ -23,7 +25,7 @@ public class NormalCMovie extends NormalController{
 
 
     /**
-     * Should be only called when the movie name <mn> exists in the data base
+     * Should be only called when the movie name <movieName> exists in the database
      * @param movieName the name of the movie.
      * @return a String with movie name, movie link, number of likes
      */
@@ -32,35 +34,33 @@ public class NormalCMovie extends NormalController{
     }
 
     /**
-     * Should be only called when the movie name <mn> exists in the data base
-     * When given a moviename, return the review of the movie.
+     * Should be only called when the movie name <movieName> exists in the database
+     * When given a movieName, return the review of the movie.
      * @param movieName the name of the movie.
      * @return an arraylist of arrays, where each array stores information of a single review in the form of
-     *         [username of reviewer, moviename, reviewContent, numCoin, ID].
+     *         [username of reviewer, movieName, reviewContent, numCoin, ID].
      */
     public ArrayList<Object[]> movieReviews(String movieName) {
         return InstanceMain.getReviewManager().listRevsOfMovie(movieName);
     }
 
-
     /**
-     * Should be only called when the movie name <moviename> exists in the data base
-     * Given a String moviename, add like.
-     * return ture iff added successfully.
+     * Should be only called when the movie name <movieName> exists in the database
+     * Given a String movieName, add like.
+     * @param movieName name of the movie.
+     * @return ture iff added successfully.
      */
     public boolean likeMovie(String movieName) {
         if (InstanceMain.getUserManager().giveLike(this.currNormalName, movieName)){
-
             return InstanceMain.getMovieManager().likeMovie(movieName);
         }
-        else {
-            return false;
-        }
+        else {return false;}
     }
 
     /**
      * @return ture iff the user's playlist is empty.
      */
+    @SuppressWarnings("unchecked")
     public boolean emptyPlaylist() {
         Object[] userInfo = InstanceMain.getUserManager().getUserInfoList(this.currNormalName, "NormalUser");
         ArrayList<String> userPlaylist = (ArrayList<String>) userInfo[6];
@@ -68,31 +68,28 @@ public class NormalCMovie extends NormalController{
     }
 
     /**
-     * Given a String moviename, undo like.
-     * return ture iff added successfully.
+     * Should be only called when the movie name <movieName> exists in the database
+     * Given a String movieName, undo like.
+     * @param movieName name of the movie.
+     * @return ture iff added successfully.
      */
+    @SuppressWarnings("unchecked")
     public boolean undoLike(String movieName) {
         Object[] userInfo = InstanceMain.getUserManager().getUserInfoList(this.currNormalName, "NormalUser");
         ArrayList<String> userPlaylist = (ArrayList<String>) userInfo[6];
         if (userPlaylist.contains(movieName)){
-
             return InstanceMain.getUserManager().undoLike(this.currNormalName, movieName) && InstanceMain.getMovieManager().undolikeMovie(movieName);
         }
-        else{
-            return false;
-        }
+        else {return false;}
     }
 
     /**
-     * add a review when provided with moviename of the movie and review content
+     * add a review when provided with movieName of the movie and review content
      * @param movieName name of the movie
      * @param revContent content of review
      * @return ture iff a review is successfully added. false otherwise
      */
     public boolean writeReview(String movieName, String revContent) {
-        return InstanceMain.getReviewManager().writeNewReview(this.currNormalName, movieName, revContent, 0);
-
-    }
-
+        return InstanceMain.getReviewManager().writeNewReview(this.currNormalName, movieName, revContent, 0);}
 
 }
