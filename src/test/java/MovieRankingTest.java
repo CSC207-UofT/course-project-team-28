309c8188efd82592d1be0940e21f3e25d3978d21
yuxuanli01 <dorislyx.li@mail.utoronto.ca>
2021-12-08 19:38:27 -0500

    updated MovieRankingTest, deleted some unnecessary codes, and changed some variables.

--- src/test/java/MovieRankingTest.java
+++ src/test/java/MovieRankingTest.java
@@ -1,20 +1,11 @@
-
-import Framework.DataAccess.WriteMovie;
-import Framework.DataAccess.WriteReview;
-import Framework.DataAccess.WriteUser;
-
-import InterfaceAdapter.InstanceMain;
 import UseCase.MovieRanking;
 import UseCase.MovieManager;
 import UseCase.ReviewManager;
 import Entity.Movie;
 
-import org.junit.After;
 import org.junit.BeforeClass;
 import org.junit.Test;
 
-import java.nio.file.FileSystems;
-import java.nio.file.Path;
 import java.util.ArrayList;
 
 
@@ -22,14 +13,12 @@ import static org.junit.Assert.assertEquals;
 
 public class MovieRankingTest {
     private static MovieRanking mr;
-    private static ReviewManager rm;
-    private static MovieManager mm;
 
 
     @BeforeClass
     public static void setUp() {
-        rm = new ReviewManager();
-        mm = new MovieManager(rm);
+        ReviewManager rm = new ReviewManager();
+        MovieManager mm = new MovieManager(rm);
         mm.addMovie("Apple", "shdjhadshjasfhkasf", "Anime", 1);
         mm.addMovie("Water", "hshauihasjhjk", "Action", 0);
         mm.addMovie("Bananaa", "hshauihasjhjkww", "Action", 2);
