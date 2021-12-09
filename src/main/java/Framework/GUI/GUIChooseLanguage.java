c70eb9fecbaa6b092803cb9d3e807585798ac5f4
SayaJPF <joanna.pan@mail.utoronto.ca>
2021-12-08 20:03:18 -0500

    GUIChooseLanguage added javadoc

--- src/main/java/Framework/GUI/GUIChooseLanguage.java
+++ src/main/java/Framework/GUI/GUIChooseLanguage.java
@@ -11,11 +11,19 @@ import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.io.IOException;
 
+/**
+ * This is the first page of our program, which allows the user to select language (English or Chinese).
+ */
 public class GUIChooseLanguage extends View{
     private String Language;
-    private JButton english;
-    private JButton mandarin;
-    private JPanel p1;
+    private final JButton english;
+    private final JButton mandarin;
+    private final JPanel p1;
+
+    /**
+     * The constructor of this class.
+     * @param previous The parameter from the super class View. It is used to switch pages.
+     */
 
     public GUIChooseLanguage(View previous){
         super(previous);
@@ -25,6 +33,10 @@ public class GUIChooseLanguage extends View{
         PlaceThingsOnP1(p1);
     }
 
+    /**
+     * Add components to panel.
+     * @param p1 The Jpanel to be modified.
+     */
     private void PlaceThingsOnP1(JPanel p1){
         p1.setLayout(null);
         p1.setBounds(20, 20, 160, 360);
@@ -57,13 +69,20 @@ public class GUIChooseLanguage extends View{
         p1.add(mandarin);
     }
 
+    /**
+     * This is the action when the user press the 'English' button.
+     * @param e An action for the click on the button.
+     */
     private void OnClickButtonEng(ActionEvent e) throws IOException {
         Language = "ENGLISH";
         GUIMain g = new GUIMain(this);
         nextView(g, false);
         this.getFrame().dispose();
     }
-
+    /**
+     * This is the action when the user press the '中文' button.
+     * @param e An action for the click on the button.
+     */
     private void OnClickButtonMan(ActionEvent e) throws IOException {
         Language = "MANDARIN";
         GUIMain g = new GUIMain(this);
@@ -71,21 +90,32 @@ public class GUIChooseLanguage extends View{
         this.getFrame().dispose();
     }
 
+    /**
+     * The getter method for getting the selected language.
+     */
     public String getLanguage(){
         return this.Language;
     }
 
+    /**
+     * The getter method for getting the text from the presenter layer.
+     */
     public TextPresenter getTextPresenter() throws IOException {
         TextPresenter textPresenter = new TextPresenter(this.Language);
         return textPresenter;
     }
 
-
+    /**
+     * The method from supper class.
+     */
     @Override
     protected void UpdateText() {
 
     }
 
+    /**
+     *The method in super class for generating a new frame.
+     */
     @Override
     protected JFrame getFrame() {
         JFrame jFrame = super.getFrame();
@@ -97,6 +127,9 @@ public class GUIChooseLanguage extends View{
         return jFrame;
     }
 
+    /**
+     *This is the main method.
+     */
     public static void main(String[] args) throws IOException {
         InstanceMain.setClearInstance();
         WriteUser writeUser = new WriteUser();
